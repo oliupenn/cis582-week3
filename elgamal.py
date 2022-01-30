eed34b45bf08aab45a13db24e1a290cae1589af5
oliupenn <52123655+oliupenn@users.noreply.github.com>
2022-01-30 18:20:57 +0800

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -11,11 +11,13 @@ def keygen():
     return pk,sk
 
 def encrypt(pk,m):
+    q = (p - 1) / 2
     r = random.randrange(1,q,1)
     c1 = pow(g,r,p)
     c2 = pow((pow(pk,r,p)*pow(m,1,p)),1,p)
     return [c1,c2]
 
 def decrypt(sk,c):
-    m = pow((pow(c[1],1,p)*pow(c[0],-sk,p)),1,p)
+    a = sk
+    m = pow(pow(c[1],1,p)*pow(c[0],-a,p),1,p)
     return m

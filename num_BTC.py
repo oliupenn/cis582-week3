66cfbda03c43e6beac3ba674a578dc4a721c0584
oliupenn <52123655+oliupenn@users.noreply.github.com>
2022-02-12 17:48:13 +0800

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -1,7 +1,12 @@
 import math
 
 def num_BTC(b):
-    c = 50 * b
+    quotient = int(b / 210000)
+    remainder = b % 210000
+    c = 0
+    reward = 50
+    for i in range(quotient):
+        c += 210000 * reward
+        reward *= 0.5
+    c += remainder * reward
     return c
-
-

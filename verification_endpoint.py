b101c63f7f6e6bae0f15a18ec393f91f386335f5
oliupenn <52123655+oliupenn@users.noreply.github.com>
2022-03-05 00:14:04 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -8,11 +8,42 @@ app = Flask(__name__)
 api = Api(app)
 app.url_map.strict_slashes = False
 
-@app.route('/verify', methods=['GET','POST'])
+
+@app.route('/verify', methods=['GET', 'POST'])
 def verify():
-    result = True
+    content = request.get_json(silent=True)
+    print(content)
+    message = cnt['payload']['message']
+    platform = cnt['payload']['platform']
+    # Check if signature is valid
+
+    if platform == 'Ethereum':
+        eth_account.Account.enable_unaudited_hdwallet_features()
+        acct, mnemonic = eth_account.Account.create_with_mnemonic()
+
+        eth_pk = acct.address
+        eth_sk = acct.key
+
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
+        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg, eth_sk)
+
+        print(eth_sig_obj.messageHash)
+        if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj.signature.hex()) == eth_pk:
+            result = True
+        else:
+            result = False
+
+    elif platform == 'Algorand':
+        algo_sk, algo_pk = algosdk.account.generate_account()
+        algo_sig_str = algosdk.util.sign_bytes(message.encode('utf-8'), algo_sk)
+
+        if algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk):
+            result = True
+        else:
+            result = False
 
     return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(port='5002')

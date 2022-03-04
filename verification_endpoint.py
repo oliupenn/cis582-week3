36ead785a56da6d530ffeb8b983d1a7679e587f9
oliupenn <52123655+oliupenn@users.noreply.github.com>
2022-03-05 00:24:54 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -20,6 +20,8 @@ def verify():
     platform = data['payload']['platform']
     # Check if signature is valid
 
+    result = False
+
     if platform == 'Ethereum':
         eth_account.Account.enable_unaudited_hdwallet_features()
         acct, mnemonic = eth_account.Account.create_with_mnemonic()
@@ -30,11 +32,8 @@ def verify():
         eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
         eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg, eth_sk)
 
-        print(eth_sig_obj.messageHash)
         if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj.signature.hex()) == eth_pk:
             result = True
-        else:
-            result = False
 
     elif platform == 'Algorand':
         algo_sk, algo_pk = algosdk.account.generate_account()
@@ -42,8 +41,6 @@ def verify():
 
         if algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk):
             result = True
-        else:
-            result = False
 
     return jsonify(result)
 

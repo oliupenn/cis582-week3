b6c5a027ff01eede99f31e63c89ff0bf47b00239
oliupenn <52123655+oliupenn@users.noreply.github.com>
2022-03-05 12:47:16 +0800

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -23,15 +23,15 @@ def send_tokens( receiver_pk, tx_amount ):
     tx_fee = params.min_fee
     last_valid_round = params.last
 
-    sender_sk = 'PBqeVXN/PUQBp7m62XoECBzJaaM5ieDcxmaLmBbnRy2svm4CCwLNIbDCLJhWf/4aUlg6XaX7yqor9lyvtY30LA=='
-    sender_mnemonic = mnemonic.from_private_key(sender_sk)
-    sender_pk = mnemonic.to_public_key(sender_mnemonic)
-    sender_address = account.address_from_private_key(sender_pk)
+    sender_sk = 't+uEbYWYTQomhqPE0jKXBhxnbKX0nF97GFfvrlEbjCq23b0IhQGk2qnTi5xvEqgLulgXc9MtT+YQJCBFkfJ/UQ=='
+    sender_address = 'W3O32CEFAGSNVKOTROOG6EVIBO5FQF3T2MWU7ZQQEQQELEPSP5IYXYZZ7E'
     receiver_address = account.address_from_private_key(receiver_pk)
     txn = transaction.PaymentTxn(sender_address, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver_address, tx_amount)
+
     signed_txn = txn.sign(sender_sk)
-    signed_txn.dictify()
-    txid = txn.get_txid()
+
+    txid = acl.send_transaction(signed_txn)
+
     return sender_pk, txid
 
 # Function from Algorand Inc.
@@ -50,10 +50,10 @@ def wait_for_confirmation(client, txid):
     print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
     return txinfo
 
-# if __name__ == '__main__':
-#     private_key, address = account.generate_account()
-#     mnemonic_secret = "YOUR MNEMONIC HERE"
-#     sk = mnemonic.to_private_key(mnemonic_secret)
-#     pk = mnemonic.to_public_key(mnemonic_secret)
-#     print("Private key:", private_key)
-#     print("Address:", address)
+if __name__ == '__main__':
+    private_key, address = account.generate_account()
+    mnemonic_secret = mnemonic.from_private_key(private_key)
+    sk = mnemonic.to_private_key(mnemonic_secret)
+    pk = mnemonic.to_public_key(mnemonic_secret)
+    print("Private key:", private_key)
+    print("Address:", address)
